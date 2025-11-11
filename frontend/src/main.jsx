11dbb0a13d8b877802b40ee6504c3bb2af398126
Vietle290 <lehoangviet2970@gmail.com>
2025-11-11 08:58:20 +0700

    vite config

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,11 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
-import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <HashRouter>
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </HashRouter>
+  </BrowserRouter>
 );
